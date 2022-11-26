73d00a2613fbc49f7b4628d75ded0779ac46cfe0
hellojquinn <hello.jquinn@gmail.com>
2022-11-26 19:18:02 +1100

    playRound function added to determine selections to equal which outcome

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # rock-paper-scissors
 The Odin Project - Rock, Paper, Scissors game
 
-1. Understand
 
 Rock, paper, scissors.
 
 A game where the user plays against the computer by selecting one of three choices where any one choice has a specific strength or weakness against another. Both the user and the computer select a choice at the same time, and whoevers choice is the stronger one in comparison to the opposing choice being weaker, wins the game.
 
-2. Plan
 
 Pseudocode:
 
@@ -15,6 +13,3 @@ Get both the users choice and the computers choice and match together
 If users choice is greater than computers choice, display message You Win
 Else if computers choice is greater than users choice, display message You Lose
 Provide input for New Game? If yes, re-run code
-
-3. Divide
-
--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 function getComputerChoice() {
-
-const choice = ['rock', 'paper', 'scissors'];
-
-return choice[Math.floor(Math.random() * choice.length)];
+    const choice = ['rock', 'paper', 'scissors'];
+    return choice[Math.floor(Math.random() * choice.length)];
 
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == 'rock' && computerSelection == 'scissors' ||
+        playerSelection == 'paper' && computerSelection == 'rock' ||
+        playerSelection == 'scissors' && computerSelection == 'paper')
+        return "You Win!"
+    else if 
+        (playerSelection == 'rock' && computerSelection == 'paper' ||
+        playerSelection == 'paper' && computerSelection == 'scissors' ||
+        playerSelection == 'scissors' && computerSelection == 'rock')
+        return "You Lose!"
+    else if
+        (playerSelection == 'rock' && computerSelection == 'rock' ||
+        playerSelection == 'paper' && computerSelection == 'paper' ||
+        playerSelection == 'scissors' && computerSelection == 'scissors')
+        return "Draw!"
+        }
+
+const playerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
