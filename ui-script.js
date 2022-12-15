e02d0ac622f4eb25d9787b83285aec4a4ee7fd6b
hellojquinn <hello.jquinn@gmail.com>
2022-12-15 23:11:30 +1100

    Basic html/css layout for warrior, wizard, archer. No JS yet

---
+++ ui-script.js
@@ -0,0 +1,14 @@
+/*
+Knight Wizard Archer
+Array that holds the options
+3 buttons, one for each option
+When the user selects one of those options, I need the computer to do the same randomly
+Options to weigh up which is greater
+Display which option is greater, announce who is winner, user or computer
+Make counter for whichever user first gets to 5 wins, announce them as winner
+Give button to restart game
+*/
+
+
+const choices = ['knight', 'wizard', 'archer'];
+
